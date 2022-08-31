9ad95925fe5d5750c5d4ffdabbc3c5d521f5353b
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-08-31 12:21:49 -0300

    feat: adicionado template do corpo do site e do box para formularios

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,39 @@
+import './Header.css';
+import { FaUserCircle, FaBars } from 'react-icons/fa';
+import { Dropdown } from 'react-bootstrap';
+import { useContext } from 'react';
+import { MenuContext } from '../../Contexts/MenuContext/MenuContext';
+
+const Header = () => {
+    const { open, setOpen } = useContext(MenuContext);
+    const user = {
+        name: 'Rawlinson'
+    };
+
+    return(
+        <div className="bg-light shadow pt-2 pb-2 ps-2 d-flex">
+            <div class="col-1 text-center">
+                <button className='btn' onClick={()=>setOpen(!open)} aria-expanded={open} aria-controls="collapseMenu">
+                    <FaBars size={20} />
+                </button>
+            </div>
+            <div className='col align-self-center'>
+                <h4 className="text-dark">Web Acadêmico</h4>
+            </div>
+            <div class="col text-end pe-4">
+                <Dropdown>
+                    <Dropdown.Toggle className='btn'  variant="" id="dropdownUser">
+                        Bem vindo, {user.name} <FaUserCircle size={30} />
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="">Perfil</Dropdown.Item>
+                        <Dropdown.Item href="/">Sair</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+        </div>   
+    );
+};
+
+export default Header;
---
+++ src/Contexts/MenuContext/MenuContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react';
+
+export const MenuContext = createContext();
+
+export const MenuContextProvider = ({children}) => {
+    const [open, setOpen] = useState(true);
+
+
+    return (
+        <MenuContext.Provider
+            value={{
+                open, setOpen
+            }}>
+            {children}
+        </MenuContext.Provider>
+    )
+}
