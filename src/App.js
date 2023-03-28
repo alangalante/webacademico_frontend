b0c95f18385b9fb27790f0ad3484d63cb2582dcf
victorcesar2 <vitor.c.j@hotmail.com>
2023-03-28 20:20:48 -0300

    Revert "Mudanças nos nomes das classes"
    This reverts commit 2510be425e798f1462a4551ced1a27f978ee1345.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CadastroUsuario from './Pages/CadastroUsuario/CadastroUsuario'
-import CadastroCurso from './Pages/CadastroCurso/CadastroCurso'
+import Cadastro_Usuario from './Pages/Cadastro_Usuario/Cadastro_Usuario'
+import Cadastro_Curso from './Pages/Cadastro_Curso/Cadastro_Curso'
 import Disciplina from "./Pages/Disciplina/Disciplina";
 import Example from "./Pages/Example/Example";
 import Header from "./Components/Header/Header";
 import Horario from "./Pages/Horario/Horario";
 import Menu from "./Components/Menu/Menu";
 import Professor from "./Pages/Professor";
-import GradeCurricular from "./Pages/GradeCurricular/GradeCurricular";
-import TemplateListar from "./Pages/Example/Template_Listar";
-import PeriodoLetivo from "./Pages/PeriodoLetivo/PeridoLetivo";
+import GradeCurricular from "./Pages/Grade_Curricular";
+import Template_Listar from "./Pages/Example/Template_Listar";
+import Periodo_Letivo from "./Pages/Periodo_Letivo/Perido_Letivo";
 
 function App() {
   return (
@@ -25,16 +25,16 @@ function App() {
               <div className="d-flex flex-wrap">
                 <Routes>
                   <Route path="/dashboard" element={<Example />} />
-                  <Route path="/curso" element={<CadastroCurso />} />
+                  <Route path="/curso" element={<Cadastro_Curso />} />
                   <Route
                     path="/grade_curricular"
                     element={<GradeCurricular />}
                   />
-                  <Route path="/periodo_letivo" element={<PeriodoLetivo />}/>
-                  <Route path="/usuario" element={<CadastroUsuario />} />
+                  <Route path="/periodo_letivo" element={<Periodo_Letivo />}/>
+                  <Route path="/usuario" element={<Cadastro_Usuario />} />
                   <Route
                     path="/template_listar"
-                    element={<TemplateListar />}
+                    element={<Template_Listar />}
                   />
                   <Route path="/horario" element={<Horario />} />
                   <Route path="/disciplina" element={<Disciplina />} />
