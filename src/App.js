90ee3edcf5532f717a0f67421be885fca8a38cce
Victor Cesar <54184972+victorcesar2@users.noreply.github.com>
2023-03-29 19:18:30 -0300

    Feat/web_perido_academico (#11)
    * feat-web-periodo-letivo
    
    * Mudanças nos nomes das classes
    
    * mudanca
    
    * tentando
    
    * Revert "mudanca"
    
    This reverts commit 0c38375e72d4b3d0776917c7a4247c9c72a2b324.
    
    * Revert "Mudanças nos nomes das classes"
    
    This reverts commit 2510be425e798f1462a4551ced1a27f978ee1345.
    
    * Revert "feat-web-periodo-letivo"
    
    This reverts commit 5d6f201298c5cc1d8fe1c4ab865db0a80b0255f2.
    
    * correções
    
    Correções para merge
    
    ---------
    
    Co-authored-by: Alan Carvalho Galante <alancarvalho@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cadastro_Usuario from "./Pages/Cadastro_Usuario/Cadastro_Usuario";
-import Cadastro_Curso from "./Pages/Cadastro_Curso/Cadastro_Curso";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CadastroUsuario from './Pages/CadastroUsuario/CadastroUsuario'
+import CadastroCurso from './Pages/CadastroCurso/CadastroCurso'
 import Disciplina from "./Pages/Disciplina/Disciplina";
 import Example from "./Pages/Example/Example";
 import Header from "./Components/Header/Header";
 import Horario from "./Pages/Horario/Horario";
 import Menu from "./Components/Menu/Menu";
 import Professor from "./Pages/Professor/index";
-import GradeCurricular from "./Pages/Grade_Curricular";
-import Template_Listar from "./Pages/Example/Template_Listar";
+import GradeCurricular from "./Pages/GradeCurricular/GradeCurricular";
+import TemplateListar from "./Pages/Example/TemplateListar";
+import PeriodoLetivo from "./Pages/PeriodoLetivo/PeridoLetivo";
 import Aluno from "./Pages/Aluno/Aluno";
 import PeriodoLetivo from "./Pages/Periodo_Letivo";
 import Turma from "./Pages/Turma";
@@ -28,15 +29,16 @@ function App() {
               <div className="d-flex flex-wrap">
                 <Routes>
                   <Route path="/dashboard" element={<Example />} />
-                  <Route path="/curso" element={<Example />} />
+                  <Route path="/curso" element={<CadastroCurso />} />
                   <Route
-                    path="/grade_curricular"
+                    path="/gradecurricular"
                     element={<GradeCurricular />}
                   />
-                  <Route path="/usuario" element={<Cadastro_Usuario />} />
+                  <Route path="/periodoletivo" element={<PeriodoLetivo />}/>
+                  <Route path="/usuario" element={<CadastroUsuario />} />
                   <Route
-                    path="/template_listar"
-                    element={<Template_Listar />}
+                    path="/templatelistar"
+                    element={<TemplateListar />}
                   />
                   <Route path="/horario" element={<Horario />} />
                   <Route path="/disciplina" element={<Disciplina />} />
